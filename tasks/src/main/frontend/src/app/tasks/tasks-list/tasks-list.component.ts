e5acf0d8fd1d9c25a2b175f7eca52639474819fa
Ferenc Bondar <fhery021@gmail.com>
2020-01-27 01:07:49 +0200

    worked on tasks UI

--- tasks/src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ tasks/src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,3 +1,4 @@
+import { Task } from './../task.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksListComponent implements OnInit {
 
+  tasks: Task[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.tasks.push(new Task(1, 'one', true, '2019.01.26'));
+    this.tasks.push(new Task(2, 'second task', false, '2019.01.27'));
+    this.tasks.push(new Task(1, 'third task', false, '2019.01.30'));
   }
 
+  getDueDateBadge(task: Task) {
+    return task.completed ? 'badge-success' : 'badge-primary';
+  }
+
+  onTaskChange(checked, task) {
+    // this.taskService.saveTask(task, event.target.checked).subscribe();
+    console.log('event and the task');
+    console.log(checked);
+    console.log(task);
+
+  }
 }
